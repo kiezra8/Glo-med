73c136527d99b09a3c58a0603c26bd18315709e1
kiezra8 <israelezrakisakye@gmail.com>
2026-02-07 11:59:44 +0000

    save button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'glomed-v5';
+const CACHE_NAME = 'glomed-v6-mobile-fix-final';
 const ASSETS = [
     './index.html',
     'https://cdn.tailwindcss.com',
@@ -10,7 +10,12 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+    self.skipWaiting();
     e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
