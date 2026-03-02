21c334aead6b30887bd4680f4e159c130c51c877
kiezra8 <israelezrakisakye@gmail.com>
2026-03-02 10:11:38 +0000

    white page fixation

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 // Register service worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then((registration) => {
+        navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`).then((registration) => {
             console.log('SW registered: ', registration);
         }).catch((registrationError) => {
             console.log('SW registration failed: ', registrationError);
